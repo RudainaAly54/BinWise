6b5f76c06108d5a4961257b6a6d3260e1ead2207
Nourseen Saeed <nourseensaeed7@gmail.com>
2025-12-01 04:34:51 +0200

    d

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_BACKEND_URL,
+        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
