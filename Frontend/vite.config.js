e794298a687d8a0ca871df3eeb395f0479b71df3
Nourseen Saeed <nourseensaeed7@gmail.com>
2025-12-01 04:32:58 +0200

    p

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,14 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(),tailwindcss()],
-  
-// })
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -18,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target:`${import.meta.env.VITE_BACKEND_URL}`, // ← your Express backend
+        target: import.meta.env.VITE_BACKEND_URL,
         changeOrigin: true,
         secure: false,
       },
