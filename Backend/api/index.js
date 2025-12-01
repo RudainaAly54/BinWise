626286ad7aa2f3e1067aa5be017bf7fbeccaddb6
Nourseen Saeed <nourseensaeed7@gmail.com>
2025-12-01 16:58:13 +0200

    g

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -24,6 +24,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://bin-wise-recycle.vercel.app", // ✅ Your PRODUCTION URL
+  "https://bin-wise-recycle-git-main-nourseens-projects.vercel.app"
 ];
 
 app.use(
