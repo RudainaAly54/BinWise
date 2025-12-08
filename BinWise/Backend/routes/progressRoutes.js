21b4eb32acf0f5fbd02fe00ca0f6367155a7c439
Nourseen Saeed <nourseensaeed7@gmail.com>
2025-12-08 03:47:46 +0200

    c

--- BinWise/Backend/routes/progressRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { updateDailyProgress } from "../controllers/progressController.js"; // keep logic in controller
-
-const router = express.Router();
-
-// Define route
-router.put("/progress", updateDailyProgress);
-
-export default router; // now default export works with import progressRoutes
